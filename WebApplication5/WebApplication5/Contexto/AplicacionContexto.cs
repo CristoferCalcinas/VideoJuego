1f2eb239b8789c28136f56a5851594949fce3036
adrian <adrian@google.com>
2023-10-23 10:57:19 -0400

    agregando contexto

---
+++ WebApplication5/WebApplication5/Contexto/AplicacionContexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication3.Contexto
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto(DbContextOptions<AplicacionContexto> options):base(options) { }
+        public DbSet<Email> email { get; set; }
+        public DbSet<Usuario> usuario { get; set; }
+        public DbSet<VideoJuegos> videoJuego { get; set; }
+    }
+}
