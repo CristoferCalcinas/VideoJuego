bc6b213e001d1471cd6e109ce627d1aad6f03a65
adrian <adrian@google.com>
2023-10-23 10:59:39 -0400

    agregando usuario controller

---
+++ WebApplication5/WebApplication5/Controllers/UsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Contexto;
+using WebApplication4.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly AplicacionContexto _aplicacionContexto;
+        
+        public UsuarioController(ILogger<UsuarioController> logger, AplicacionContexto aplicacionContexto)
+        {
+            _logger = logger;
+            _aplicacionContexto = aplicacionContexto;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Usuario> GET()
+        {
+            return _aplicacionContexto.usuario.ToList();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult POST([FromBody] Usuario newDocente)
+        {
+            _aplicacionContexto.usuario.Add(newDocente);
+            _aplicacionContexto.SaveChanges();
+            return Ok(newDocente);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IActionResult PUT([FromBody] Usuario docenteUpdate)
+        {
+            _aplicacionContexto.usuario.Update(docenteUpdate);
+            _aplicacionContexto.SaveChanges();
+            return Ok(docenteUpdate);
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public IActionResult DELETE(int idUsuarioDelete)
+        {
+            _aplicacionContexto.usuario.Remove(_aplicacionContexto.usuario.ToList().Where(x=>x.IdUsuario == idUsuarioDelete).FirstOrDefault());
+            _aplicacionContexto.SaveChanges();
+            return Ok(idUsuarioDelete);
+        }
+    }
+}
